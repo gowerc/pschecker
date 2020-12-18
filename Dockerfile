a8a6db6c6baaf121070fd33783ec564757f6b460
gowerc <craiggower@gmail.com>
2020-12-18 18:38:44 +0000

    initial

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM python:3.9-slim
+
+RUN apt-get -y update
+
+# Install utilities
+RUN apt-get install -y wget gnupg curl unzip
+
+# Install google chrome
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+RUN apt-get -y update && apt-get install -y google-chrome-stable 
+
+# Install chromedriver for selinium
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+
+# Set display port to avoid crash
+ENV DISPLAY=:99
+
+ADD ./ /project
+
+# Install python dependencies
+RUN python3.9 -m pip install -r /project/requirements.txt
+
+CMD ["python3.9", "/project/pschecker.py"]
---
+++ pschecker.py
@@ -0,0 +1,96 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+driver = webdriver.Chrome(options=chrome_options)
+
+results = {}
+
+PAGETIMEOUT = 12
+
+try:
+    driver.get("https://www.amazon.co.uk/dp/B08H95Y452")
+    element = WebDriverWait(driver, PAGETIMEOUT).until(
+        EC.presence_of_element_located((By.ID, "outOfStock"))
+    )
+    instock_amazon = False
+except TimeoutException:
+    instock_amazon = True
+finally:
+    results["Amazon"] = instock_amazon
+
+
+try:
+    driver.get("https://www.game.co.uk/playstation-5")
+    element = WebDriverWait(driver, PAGETIMEOUT).until(
+        EC.presence_of_element_located((By.ID, "contentPanels3"))
+    )
+    game_text = element.\
+        find_element_by_class_name("sectionButton").\
+        find_element_by_tag_name("a").\
+        text.\
+        upper()
+    instock_game = False if game_text == "OUT OF STOCK" else True
+except TimeoutException:
+    instock_game = True
+finally:
+    results["GAME"] = instock_game
+
+
+try:
+    driver.get("https://www.argos.co.uk/product/8349000")
+    element = WebDriverWait(driver, PAGETIMEOUT).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "add-to-trolley-main"))
+    )
+    argos_text = element.\
+        find_element_by_tag_name("button").\
+        text.\
+        replace("\n", " ").\
+        upper()
+    instock_argos = True if argos_text == "ADD TO TROLLEY" else False
+except TimeoutException:
+    instock_argos = False
+finally:
+    results["Argos"] = instock_argos
+
+
+try:
+    driver.get("https://www.johnlewis.com/browse/electricals/gaming/playstation/console/_/N-2pehZ1z0pdgi")
+    element = WebDriverWait(driver, PAGETIMEOUT).until(
+        EC.presence_of_element_located((By.XPATH, "//section[@data-product-id='5115192']"))
+    )
+    jl_text = element.text.upper()
+    instock_jl = False if "OUT OF STOCK" in jl_text else True 
+except TimeoutException:
+    instock_jl = False
+finally:
+    results["John Lewis"] = instock_jl
+
+
+try:
+    driver.get("https://www.currys.co.uk/gbuk/sony-ps5/console-gaming/consoles/634_4783_32541_49_ba00013671-bv00313579/xx-criteria.html")
+    element = WebDriverWait(driver, PAGETIMEOUT).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "resultList"))
+    )
+    instock_currys = False if "NO RESULTS WERE FOUND FOR YOUR SEARCH" in element.text.upper() else True
+except TimeoutException:
+    instock_currys = False
+finally:
+    results["Currys"] = instock_currys
+
+
+driver.close()
+
+
+print(results)
+
